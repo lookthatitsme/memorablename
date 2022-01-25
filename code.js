d3bb23c72c8a0c0ccffdd2318bcd4adb814eb70b
lookthatitsme <98414856+lookthatitsme@users.noreply.github.com>
2022-01-25 12:42:52 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,67 @@
+var spaceX = 0;
+var spaceY = 0;
+function ui {
+var space = spaceX + spaceY;
+}
+case KeyEvent.KEY_PRESSED:
+  if (ke.getKeyCode() == KeyEvent.VK_W) {
+    wPressed = true;
+  }
+if(wPressed = true) {
+console.log("w is true");
+}
+if(aPressed = true) {
+console.log("a is true");  
+}
+if(sPressed = true) {
+  console.log("s is true");
+  }
+///////servers//////
+var data = [162.198.0.1,198.162.1.1,82.123.529,0.78.1.03]
+
+/////number #1 server is 162.198.0.1          
+/////number #2 server is 192.162.1.1 
+/////number #3 server is 82.123.529
+/////number #4 server is 0.78.1.03
+
+
+//////setting up server////////////
+html([getserver{"data"}]) {
+html.connect({var data = [0,1,2,3];});
+data[2] = server2.connect;
+console.log("ping" + data[2]);
+DNSl = find[data[2].DNS];
+newWindows.open("https://" + DNSl);
+}
+console.clear();
+getnewserver(getdoc.id("server");)
+if (getnewserver = false) { 
+  console.log("Sorry couldn't connect to the server number #2");
+}
+if (getnewsever = true) 
+}
+html([getserver{"data"}]) {
+html.connect({var data = [0,1,2,3];});
+data[2] = server2.connect;
+console.log("ping" + data[2]);
+DNSl = find[data[2].DNS];
+newWindows.open("https://" + DNSl);
+}
+/////////////////////////////////////////sorry aviable servers are null///////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
